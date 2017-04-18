0a322897097e644fef9f63264a960d5f4603375e
Joran Dob <joran998@gmail.com>
2017-04-18 17:21:14 +0200

    Edit crawler.php

--- libs/crawler.php
+++ libs/crawler.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-type:application/json;charset=utf-8');
-ini_set( 'error_reporting', E_ALL );
-ini_set( 'display_errors', true );
+//ini_set( 'error_reporting', E_ALL );
+//ini_set( 'display_errors', true );
 
 
 //base url
